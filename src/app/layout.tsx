e4849df3d3cac1a1f574821f7498d743ba60fcea
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2025-08-11 11:04:50 -0300

    fix: force dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
-      className="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
+      className="dark text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
     >
       <head>
         <title>Surfpool Studio</title>
